40fbf9c21ca400679ed569c4a1d903ed6de3a3b2
Shivam Gujar <shivamgujar06@gmail.com>
2025-12-30 15:39:40 +0530

    Uploaded Searching Techniques

---
+++ arrays/searching-techniques/LinearSearch.java
@@ -0,0 +1,27 @@
+class LinearSearch {
+
+    // Returns index of target if found, else -1
+    static int linearSearch(int[] arr, int target) {
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 25, 30, 45, 50};
+        int target = 30;
+
+        int result = linearSearch(arr, target);
+
+        if (result == -1) {
+            System.out.println("Element not found");
+        } else {
+            System.out.println("Element found at index " + result);
+        }
+    }
+}
+
